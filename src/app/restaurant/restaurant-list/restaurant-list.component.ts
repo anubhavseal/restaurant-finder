e400fd94deccd2b8349e99c13673dceed0971815
anubhav.seal <anubhav.seal@gmail.com>
2018-11-01 11:03:27 +0530

    feat: Restaurant List Laout

--- src/app/restaurant/restaurant-list/restaurant-list.component.ts
+++ src/app/restaurant/restaurant-list/restaurant-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-restaurant-list',
-  templateUrl: './restaurant-list.component.html',
-  styleUrls: ['./restaurant-list.component.css']
+  selector: "app-restaurant-list",
+  templateUrl: "./restaurant-list.component.html",
+  styleUrls: ["./restaurant-list.component.css"]
 })
 export class RestaurantListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  tiles;
+  constructor() {
+    this.tiles = [
+      { text: "One", cols: 3, rows: 1, color: "lightblue" },
+      { text: "Two", cols: 1, rows: 2, color: "lightgreen" },
+      { text: "Three", cols: 1, rows: 1, color: "lightpink" },
+      { text: "Four", cols: 2, rows: 1, color: "#DDBDF1" },
+      { text: "One", cols: 3, rows: 1, color: "lightblue" },
+      { text: "Two", cols: 1, rows: 2, color: "lightgreen" },
+      { text: "Three", cols: 1, rows: 1, color: "lightpink" },
+      { text: "Four", cols: 2, rows: 1, color: "#DDBDF1" }
+    ];
   }
 
+  ngOnInit() {}
 }
