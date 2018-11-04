69649958df734dfe65848ec7341bf42ad7a2ad9e
anubhav <anubhav.seal@gmail.com>
2018-11-04 17:45:58 +0530

    feat: Restaurant List

--- src/app/restaurant/restaurant-list/restaurant-list.component.ts
+++ src/app/restaurant/restaurant-list/restaurant-list.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { RestaurantsFetchService } from "../../core/services/restaurants-fetch.service";
+import { Component, OnInit, HostListener } from "@angular/core";
+import { Observable } from "rxjs";
+import { Select, Store } from "@ngxs/store";
+import {
+  GetRestaurantsForSelectedCity,
+  SetSelectedRestaurant,
+  GetRestaurantReviews,
+  ClearAllRestaurantsAndSelecetdRestaurant
+} from "./restaurant-list.action";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-restaurant-list",
@@ -7,27 +15,69 @@ import { RestaurantsFetchService } from "../../core/services/restaurants-fetch.s
   styleUrls: ["./restaurant-list.component.css"]
 })
 export class RestaurantListComponent implements OnInit {
-  tiles;
-  restaurants;
-  constructor(private restaurantFetchService: RestaurantsFetchService) {
-    this.tiles = [
-      { text: "One", cols: 3, rows: 1, color: "lightblue" },
-      { text: "Two", cols: 1, rows: 2, color: "lightgreen" },
-      { text: "Three", cols: 1, rows: 1, color: "lightpink" },
-      { text: "Four", cols: 2, rows: 1, color: "#DDBDF1" },
-      { text: "One", cols: 3, rows: 1, color: "lightblue" },
-      { text: "Two", cols: 1, rows: 2, color: "lightgreen" },
-      { text: "Three", cols: 1, rows: 1, color: "lightpink" },
-      { text: "Four", cols: 2, rows: 1, color: "#DDBDF1" }
-    ];
+  restaurants = [];
+  selectedCity: any;
+
+  @Select(state => state.restaurant.restaurantList)
+  restaurants$: Observable<any>;
+  @Select(state => state.city.selectedCity)
+  city$: Observable<any>;
+
+  cityUnsubscribe;
+  restaurantUnsubscribe;
+
+  constructor(private store: Store, private router: Router) {}
+
+  @HostListener("window:scroll", ["$event"])
+  fetchNextSetOfRestaurants() {
+    if (
+      window.innerHeight + Math.ceil(window.pageYOffset) >=
+      document.body.offsetHeight - 2
+    ) {
+      this.fetchRestaurants();
+    }
+  }
+
+  selectRestaurant(selectedRestaurant) {
+    this.store
+      .dispatch(new SetSelectedRestaurant(selectedRestaurant))
+      .subscribe(() => {
+        this.store
+          .dispatch(new GetRestaurantReviews(selectedRestaurant))
+          .subscribe(() => {
+            this.router.navigate(["/details"]);
+          });
+      });
+  }
+
+  fetchRestaurants() {
+    this.store.dispatch(
+      new GetRestaurantsForSelectedCity(this.selectedCity.id)
+    );
   }
 
   ngOnInit() {
-    debugger; 
-    this.restaurantFetchService.fetchRestaurantsForSelectedCity()
-    .subscribe(restaurants => {
-      console.log('restaurants', restaurants);
-      this.restaurants = restaurants['restaurants'];
+    this.cityUnsubscribe = this.city$.subscribe(selectedCity => {
+      this.selectedCity = selectedCity;
+      this.restaurantUnsubscribe = this.restaurants$.subscribe(restaurants => {
+        if (restaurants.length === 0) {
+          this.fetchRestaurants();
+        } else {
+          this.restaurants = restaurants;
+        }
+      });
     });
+  }
+
+  ngOnDestroy() {
+    this.cityUnsubscribe.unsubscribe();
+    this.restaurantUnsubscribe.unsubscribe();
+    if (this.router.url === "/search") {
+      this.store
+        .dispatch(new ClearAllRestaurantsAndSelecetdRestaurant())
+        .subscribe(() => {
+          this.router.navigate(["/search"]);
+        });
+    }
   }
 }
